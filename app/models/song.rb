e06cc5d8d609490d35f8ec9c380b0dfac01c67c4
Winston  Chung <18.chung.winston@ctkprep.org>
2020-02-11 16:56:13 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -13,15 +13,6 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil 
   end
   
-  def genre_name=(name)
-    genre = Genre.find_or_create_by(name: name)
-    self.genre = genre
-  end
-
-  def genre_name 
-    self.genre ? self.genre.name : nil 
-  end
-  
   def note_contents=(contents)
     contents.each do |content|
       if !content.empty?
